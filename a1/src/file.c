611d810fa7e689513fa5d99874c176406494f8d7
atxxl <eatxxl@gmail.com>
2017-09-14 20:08:36 +0200

    split the main function into smaller functions

--- a1/src/file.c
+++ a1/src/file.c
@@ -3,66 +3,84 @@
 #include <string.h> // strerror.
 #include <errno.h>  // errno.
 
+
 const char * const FILE_TYPE_STRINGS[] = {
-  	"data",
-  	"empty",
-  	"ASCII text",
+  	"data",                                //0
+  	"empty",                               //1
+  	"ASCII text",                          //2
+    "ISO-8859 text",                       //3
+    "UTF-8 Unicode text",                  //4
+    "Big-endian UTF-16 Unicode text",      //5
+    "Little-endian UTF-16 Unicode text",   //6
 };
 
 // Assumes: errnum is a valid error number
 int print_error(char *path, int errnum) {
     return fprintf(stdout, "%s: cannot determine (%s)\n",
-        path, strerror(errnum));
+      path, strerror(errnum));
 }
+
+int print_message(char *path, int type_num) {
+  return printf("%s: %s\n", path, FILE_TYPE_STRINGS[type_num]);
+}
+
+int empty_checker(FILE* fp) {
+  fseek(fp, 0, 2);
+  int size = ftell(fp);
+  if (size == 0) 
+    return 1;
+  else
+    return 0;
+  }
+
+int ascii_checker(FILE* fp) {
+  int c;
+  int result = 2; 
+  fseek(fp, 0, SEEK_SET);
+  while (!feof(fp)) {
+    c = fgetc(fp);
+    if(c != EOF) {
+      if (!( (c >= 0x07 && c <= 0x0D) || c == 0x1B || 
+        (c >= 0x20 && c <= 0x7E) )) {
+        
+        result = 0;
+        break;
+      } 
+    }
+  }
+  return result;
+}
+
+void type_detector(FILE* fp, char *path) {
+  if (empty_checker(fp) == 1) {
+    print_message(path, 1);
+  }
+  else if (ascii_checker(fp) == 2)
+  {
+    print_message(path, 2);
+  }
+  else
+    print_message(path, 0);
+}
+
 
 int main(int argc, char *argv[]) {
   if (argc >= 2){
-    
-    int i;
-
+    int i = 1;
     while( i+1 <= argc)
     {
-      int c;
       FILE *fp;
       fp = fopen(argv[i],"r");
       
-
       if (fp == NULL){
         print_error(argv[i], errno);
         break;
         //exit(EXIT_SUCCESS);
       }
-
-        //Finds out if the file is empty
-        fseek(fp, 0, 2);
-        int size = ftell(fp);
-        if (size == 0) {
-          printf("%s: %s\n", argv[i], FILE_TYPE_STRINGS[1]);
-          //exit(EXIT_SUCCESS);
-        }
-        fseek(fp, 0, SEEK_SET);
-
-        //Finds out if the file is ASCII or Binary 
-        while (!feof(fp)) 
-        {
-          c = fgetc(fp);
-          if(c != EOF){ // End of character not included 
-          //If any non-Ascii char has been found, the file we be deemed as a data file.
-            if (!( (c >= 0x07 && c <= 0x0D) || c == 0x1B || (c >= 0x20 && c <= 0x7E) )) { 
-              printf("%s: %s\n", argv[i], FILE_TYPE_STRINGS[0]);
-              break;
-              //exit(EXIT_SUCCESS);
-            }
-            else{
-              printf("%s: %s\n", argv[i], FILE_TYPE_STRINGS[2]);
-              break;
-              //exit(EXIT_SUCCESS);             
-            }
-          }
-        }
-
-
-        i++;
+      
+      type_detector(fp, argv[i]);
+      
+      i++;
     }
     exit(EXIT_SUCCESS);
   
