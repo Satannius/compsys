ea525d3192330e8a6758a6088430b9f07faf6e22
Tobias Overlund Stannius <tobiasostannius@gmail.com>
2017-09-29 16:51:49 +0200

    Added movq_MtoR

--- a2/src/main.c
+++ a2/src/main.c
@@ -67,13 +67,13 @@ int main(int argc, char* argv[]) {
         val inst_word = memory_read_unaligned(mem, 0, 1, pc, true);
 
         // decode - here we pick apart the instruction in major and minor opcode,
-	// register specifiers and any immediate value. Next we identify all the
-	// instructions and generate control signals:
-	// For A2 you'll need to add generation of more control signals to control
-	// the rest of the machine.
-	// Some of the signals below are not used initially, but you'll have to
-	// use them to finish A2. Since they are unused, gcc will generate warnings
-	// for them, but they are put here on purpose to make your task easier.
+    // register specifiers and any immediate value. Next we identify all the
+    // instructions and generate control signals:
+    // For A2 you'll need to add generation of more control signals to control
+    // the rest of the machine.
+    // Some of the signals below are not used initially, but you'll have to
+    // use them to finish A2. Since they are unused, gcc will generate warnings
+    // for them, but they are put here on purpose to make your task easier.
         val major_op = pick_bits(4,4, inst_word);   // 0xAb, first op
         val minor_op = pick_bits(0,4, inst_word);   // 0xaB, second op
 
@@ -85,9 +85,11 @@ int main(int argc, char* argv[]) {
 
         bool is_ari = (is(ARITHMETIC, major_op));
         bool is_jcc = (is(JCC, major_op));
+        bool is_j3c = (is_jcc && !(is(0, major_op)));
 
-        bool has_regs = (is_move_rr || is_move_ir || is_ari || is_jcc);
+        bool has_regs = (is_move_rr || is_move_ir || is_ari);
         bool has_mem = (is_move_ir || is_move_rm || is_move_mr);
+        
         bool has_cmp  = (is(ARITHMETIC, major_op) && is(4, minor_op));
 
         val size = or( use_if((!has_regs && !has_mem), from_int(1)), 
@@ -102,7 +104,7 @@ int main(int argc, char* argv[]) {
         val imm_bytes = or( use_if(!has_mem, pick_bits(8, 32, inst_word)),
                             use_if( has_mem, pick_bits(16, 32, inst_word)));
         val imm = imm_bytes;
-        val sign_extended_imm = sign_extend(31,imm);
+        val sign_extended_imm = sign_extend(31, imm);
 
         // Set next instruction
         val next_inst_pc = add(pc, size);
@@ -138,20 +140,26 @@ int main(int argc, char* argv[]) {
     // For A2 you'll likely have to extend this section as there will be two
     // register updates for some instructions.
         val target_reg = reg_b;
-        val target_mem = or(use_if(!has_mem,from_int(0)),use_if(has_mem,add(imm, op_b)));
+        val target_mem = or(use_if(!has_mem, from_int(0)), use_if(has_mem, add(imm, op_b)));
 
-        bool reg_wr_enable = (is_move_rr || is_move_ir || (is_ari && !(has_cmp))|| is_jcc); 
-        bool mem_wr_enable = ( (is(MOV_RtoM, major_op)) || is_jcc);
+        bool reg_wr_enable = (is_move_rr || is_move_ir || (is_ari && !(has_cmp)) ); 
+        bool mem_wr_enable = is(MOV_RtoM, major_op);
 
         // determine PC for next cycle. Right now we can only execute next in sequence.
     // For A2 you'll have to pick the right value for branches, call and return as
     // well.
-        val next_pc = next_inst_pc;
+        val next_pc = or( use_if(!(is_jcc), next_inst_pc),
+                      or( use_if( (is_j3c && jcc_res), imm),
+                          use_if(  is_jcc, imm)) );
 
         // potentially pretty-print something to show progress before
         // ending cycle and overwriting state from start of cycle:
         // For A2, feel free to add more information you'd like for your own debugging
+    
         printf("%llx : ", pc.val);
+        printf("of %d ", cc.of);
+        printf("zf %d ", cc.zf);
+        printf("sf %d ", cc.sf);
         // printf(" reg_wr_enable %d ", reg_wr_enable);
         // printf(" mem_wr_enable %d ", mem_wr_enable);
         // printf(" imm: %llx ", imm.val);
@@ -204,9 +212,11 @@ int main(int argc, char* argv[]) {
     // need to write to an additional register for some instructions and implement
     // writing to memory.
         pc = next_pc;
+        cc = ari_res.cc;
         memory_write(regs, 1, target_reg, datapath_result, reg_wr_enable);
         memory_write(mem, 1, target_mem, datapath_result, mem_wr_enable);
         
     }
     printf("Done\n");
 }
+
