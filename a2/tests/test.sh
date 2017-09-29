038daf6ee256ba7f5184bb72b638be89453104f8
Tobias Overlund Stannius <tobiasostannius@gmail.com>
2017-09-29 19:21:13 +0200

    Fixed naming in test.sh

--- a2/tests/test.sh
+++ a2/tests/test.sh
@@ -8,10 +8,11 @@ errorcount=0
 exitcode=0
 for f in ./*.x64
 do
-    echo ">>> Testing ${f}"
-    asm=$(../architecture-tools/asm ./${f} ./${f}.out)
-    trace=$(../architecture-tools/sim ./${f}.out ${f}.trace)
-    sim=$( ../src/sim ./${f}.out ./${f}.trace)
+    file="${f%.*}"
+    echo ">>> Testing ${file}"
+    asm=$(../architecture-tools/asm ./${file}.x64 ./${file}.out)
+    trace=$(../architecture-tools/sim ./${file}.out ${file}.trace)
+    sim=$( ../src/sim ./${file}.out ./${file}.trace)
     actual=${sim:(-4)}
     expected="Done"
     if [ "${actual}" != "${expected}" ]; then
