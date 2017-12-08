0321edb09dcad0a82d6683d6eb33bce6c8c51cc4
satannius <tobiasostannius@gmail.com>
2017-12-08 15:04:03 +0100

    Added echo and process_cmd to name_server

--- a6/src/name_server.c
+++ a6/src/name_server.c
@@ -1,9 +1,43 @@
 #include <stdio.h>
+#include "csapp.h"
 #include "name_server.h"
-
 #define ARGNUM 1 // TODO: Put the number of you want to take
 
-void echo(int connfd);
+void process_cmd(char buf[])
+{
+    const char s[2] = " ";
+    char *token;
+    token = strtok(strdup(buf),s);
+
+    while(token != NULL) {
+        printf("%s\n", token);
+
+        token = strtok(NULL, s);
+    }
+    
+    printf("Received: %s", buf);
+}
+
+/*
+ * Based on csapp echo - read and echo text lines until client closes connection
+ */
+/* $begin echo */
+void echo(int connfd) 
+{
+    size_t n; 
+    char buf[MAXLINE]; 
+    rio_t rio;
+
+    Rio_readinitb(&rio, connfd);
+    while((n = Rio_readlineb(&rio, buf, MAXLINE)) != 0) { //line:netp:echo:eof
+	    printf("server received %d bytes\n", (int)n);
+        process_cmd(buf);
+	    Rio_writen(connfd, buf, n);
+    }
+
+    printf("%s", buf);
+}
+
 void *thread(void *vargp);
 
 int main(int argc, char**argv) {
@@ -13,7 +47,8 @@ int main(int argc, char**argv) {
     pthread_t tid; 
 
     if (argc != ARGNUM + 1) {
-        printf("%s expects %d arguments.\n", (argv[0]+2), ARGNUM);
+        printf("%s expects %d argument(s).\n", (argv[0]+2), ARGNUM);
+        printf("usage: ./%s <port number>\n", (argv[0]+2));
         return(0);
     }
 
