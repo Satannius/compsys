3db7f68d99dcdc03e5c09a88216cdce8105916aa
atxxl <eatxxl@gmail.com>
2017-12-08 12:40:10 +0100

    multi-threads server add

--- a6/src/name_server.c
+++ a6/src/name_server.c
@@ -1,13 +1,44 @@
 #include <stdio.h>
 #include "name_server.h"
 
-#define ARGNUM 0 // TODO: Put the number of you want to take
+#define ARGNUM 1 // TODO: Put the number of you want to take
+
+void echo(int connfd);
+void *thread(void *vargp);
 
 int main(int argc, char**argv) {
+	int listenfd, *connfdp;
+    socklen_t clientlen;
+    struct sockaddr_storage clientaddr;
+    pthread_t tid; 
+
     if (argc != ARGNUM + 1) {
         printf("%s expects %d arguments.\n", (argv[0]+2), ARGNUM);
         return(0);
     }
-    //TODO: Implement
+
+    listenfd = Open_listenfd(argv[1]);
+
+    while (1) {
+        clientlen=sizeof(struct sockaddr_storage);
+		connfdp = Malloc(sizeof(int)); //line:conc:echoservert:beginmalloc
+		*connfdp = Accept(listenfd, (SA *) &clientaddr, &clientlen); //line:conc:echoservert:endmalloc
+		Pthread_create(&tid, NULL, thread, connfdp);
+    }
+
     return 0;
 }
+
+
+/* Thread routine */
+void *thread(void *vargp) 
+{  
+    int connfd = *((int *)vargp);
+    Pthread_detach(pthread_self()); //line:conc:echoservert:detach
+    Free(vargp);                    //line:conc:echoservert:free
+    echo(connfd);
+    Close(connfd);
+    return NULL;
+}
+
+
