23b2d3765a252b5acb4e836f22c198c42a4bf4b6
atxxl <eatxxl@gmail.com>
2017-12-15 16:49:13 +0100

    building lookup(s)

--- a6/src/name_server.c
+++ a6/src/name_server.c
@@ -2,20 +2,96 @@
 #include "csapp.h"
 #include "name_server.h"
 #define ARGNUM 1 // TODO: Put the number of you want to take
+#define USERNUM 2
+
+struct list{
+    int  port;
+    int  logged_in;
+    char name[32];
+    char password[32];
+    char ip[32];
+    struct list *next;
+};
+
+static struct list user_list[2];
+
+/* https://stackoverflow.com/questions/4384359/quick-way-to-implement-dictionary-in-c 
+struct nlist *lookup(char *s)
+{
+    struct nlist *np;
+    for (np = hashtab[hash(s)]; np != NULL; np = np->next)
+        if (strcmp(s, np->name) == 0)
+          return np; //found
+    return NULL;     //not found 
+}
+*/
 
 void process_cmd(char buf[])
 {
     const char s[2] = " ";
     char *token;
-    token = strtok(strdup(buf),s);
-
-    while(token != NULL) {
-        printf("%s\n", token);
-
-        token = strtok(NULL, s);
-    }
+    token = strtok(strdup(buf), s);
+    printf("first token: %s \n", token);
     
-    printf("Received: %s", buf);
+    if (strcmp(token, "/login") == 0){
+        // verify the user and password 
+        token = strtok(NULL, s);
+        printf("token: %s \n", token);
+        for (int i = 0; i < USERNUM; i++){
+            if (user_list[i].logged_in == 0){
+                printf("This user is already logged in. \n");
+                break;
+            }
+            if (strcmp(token, user_list[i].name) == 0){
+                // continue with this user
+                printf("user %s found. \n", user_list[i].name);
+                token = strtok(NULL, s);
+                if (strcmp(token, user_list[i].password) == 0){
+                    printf("Password is correct. \n");
+                    token = strtok(NULL, s);
+                    user_list[i].port = atoi (token);
+                    printf("port: %d \n", user_list[i].port);
+                    token = strtok(NULL, s);
+                    strcpy(user_list[i].ip, token);
+                    printf("IP: %s ", token);
+                    user_list[i].logged_in = 1;
+                    break;
+                }
+            }
+        printf("Invaild user or password. \n");
+        }
+    }
+
+
+    else if (){
+        //logout
+    }
+
+    else if (strcmp(token, "/lookup\n") == 0){
+        //lookup
+    }
+
+    else if (strcmp(token, "/lookup") == 0){
+
+    }
+
+    else {
+        printf("You can either log in or exit the program. \n");
+    }
+
+    token = strtok(NULL, s);
+    printf("Received: %s \n", buf);
+}
+
+int lookup(char token){
+    int online = 0;
+    for (int i = 0; i < USERNUM; i++){
+        if (){
+            strcmp(user_list[i].name, token);
+        }
+
+    }
+
 }
 
 /*
@@ -52,15 +128,22 @@ int main(int argc, char**argv) {
         return(0);
     }
 
+    strcpy(user_list[0].name, "banana");
+    strcpy(user_list[0].password, "123456");
+    strcpy(user_list[1].name, "fisk");
+    strcpy(user_list[1].password, "654321");
+    //printf("user_list: %s\n", user_list[0].name);
+    //printf("user_list: %s\n", user_list[0].password);
+    //printf("user_list: %i\n", user_list[0].port);
+
     listenfd = Open_listenfd(argv[1]);
 
     while (1) {
-        clientlen=sizeof(struct sockaddr_storage);
-		connfdp = Malloc(sizeof(int)); //line:conc:echoservert:beginmalloc
-		*connfdp = Accept(listenfd, (SA *) &clientaddr, &clientlen); //line:conc:echoservert:endmalloc
+        clientlen = sizeof(struct sockaddr_storage);
+		connfdp   = Malloc(sizeof(int)); //line:conc:echoservert:beginmalloc
+		*connfdp  = Accept(listenfd, (SA *) &clientaddr, &clientlen); //line:conc:echoservert:endmalloc
 		Pthread_create(&tid, NULL, thread, connfdp);
     }
-
     return 0;
 }
 
