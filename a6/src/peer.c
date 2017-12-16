f6e6ae0c71377d91b802d9abd733d4e2a5c3f796
satannius <tobiasostannius@gmail.com>
2017-12-16 17:33:24 +0100

    Modified peer to handle /exit and /login partly

--- a6/src/peer.c
+++ a6/src/peer.c
@@ -2,7 +2,32 @@
 #include "csapp.h"
 #include "peer.h"
 
-#define ARGNUM 2 // TODO: Put the number of you want to take
+#define ARGNUM 0 // TODO: Put the number of you want to take
+
+void login(char args[]) {
+    printf("Establishing connection ...");
+    printf("Received args: %s", args);
+
+    int clientfd;
+    char *host, *port, buf[MAXLINE];
+    rio_t rio;
+
+    host = "localhost";
+    port = "10000";
+
+    clientfd = Open_clientfd(host, port);
+    Rio_readinitb(&rio, clientfd);
+
+    // PERFORM SPECIAL HANDSHAKE
+    // If fails: break
+
+    while (Fgets(buf, MAXLINE, stdin) != NULL) {
+        Rio_writen(clientfd, buf, strlen(buf)); // Send request
+	    Rio_readlineb(&rio, buf, MAXLINE);      // Read reply
+	    Fputs(buf, stdout);                     // Print reply
+    }
+    Close(clientfd); //line:netp:echoclient:close
+}
 
 /*
  * Based on csapp echoclient.c - An echo client
@@ -10,31 +35,35 @@
 /* $begin echoclientmain */
 int main(int argc, char **argv) 
 {
-    int clientfd;
-    char *host, *port, buf[MAXLINE];
-    rio_t rio;
-
     if (argc != ARGNUM + 1) {
         printf("%s expects %d arguments.\n", (argv[0]+2), ARGNUM);
-        fprintf(stderr, "usage: %s <host> <port>\n", argv[0]);
+        fprintf(stderr, "usage: %s \n", argv[0]);
 	    exit(0);
     }
-    host = argv[1];
-    port = argv[2];
 
-    clientfd = Open_clientfd(host, port);
-    Rio_readinitb(&rio, clientfd);
-
+    char buf[MAXLINE];
+    const char s[2] = " ";
+    char *token;
+    
     while (Fgets(buf, MAXLINE, stdin) != NULL) {
-        Rio_writen(clientfd, buf, strlen(buf));
-	    Rio_readlineb(&rio, buf, MAXLINE);
-	    Fputs(buf, stdout);
-        if (strcmp(buf, "/exit\n") == 0){
-            Fputs("Bye!\n", stdout);
+        token = strtok(strdup(buf), s);
+        printf("%s\n", token);
+        
+	    // Fputs(buf, stdout); // Echo user input.
+        if (strcmp(token, "/login") == 0){
+            Fputs("Establishing connection ...\n", stdout);
+            login(buf);
+        }
+        else if (strcmp(token, "/exit\n") == 0){
+            Fputs("Exiting ...\n", stdout);
             exit(0);
         }
+        else {
+            Fputs("Available commands:\n/login <nick> <password> <ip> <port>\n/exit\n", stdout);
+        }
     }
-    Close(clientfd); //line:netp:echoclient:close
+    
+    
     exit(0);
 }
 /* $end echoclientmain */
